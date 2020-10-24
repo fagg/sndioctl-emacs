9d5573cc7814b41b23ab6b8e23a480bf945ad83e
Ashton Fagg <ashton@fagg.id.au>
2020-10-23 20:36:27 -0400

    Typos

--- sndioctl-emacs.el
+++ sndioctl-emacs.el
@@ -1,6 +1,6 @@
-;;; sndioctl-emacs.el --- Allows OpenBSD sndio to be controlled emacs
+;;; sndioctl-emacs.el --- Allows OpenBSD sndio to be controlled in emacs
 ;;; Copyright (c) 2020 Ashton Fagg <ashton@fagg.id.au>
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; This file is NOT part of GNU emacs
 ;;
@@ -8,7 +8,7 @@
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
